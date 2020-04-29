8f4d50367c334b56ea1411ec2a4ba19f417df134
Frank <776882526@qq.com>
2020-04-29 08:43:17 +0800

    Signed-off-by: Frank <776882526@qq.com>

---
+++ src/main/java/com/ycj/fastinterface/FastinterfaceApplication.java
@@ -0,0 +1,17 @@
+package com.ycj.fastinterface;
+
+import net.hasor.spring.boot.EnableHasor;
+import net.hasor.spring.boot.EnableHasorWeb;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableHasor()
+@EnableHasorWeb()
+@SpringBootApplication
+public class FastinterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastinterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ycj/fastinterface/FastinterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycj.fastinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastinterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
